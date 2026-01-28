c6769e30420c5cb82fe776d63c6de8048193dfcc
Orazio <xabynix@gmail.com>
2026-01-28 18:04:01 +0100

    fix: add environment replacement in set-env.js

--- set-env.js
+++ set-env.js
@@ -6,13 +6,19 @@ const targetPath = path.join(__dirname, './src/environments/environment.ts');
 let envContent = fs.readFileSync(targetPath, 'utf8');
 
 const apiUrl = process.env.API_URL;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
 
-if (!apiUrl) {
-  console.error('ERRORE: La variabile API_URL non è impostata su Vercel!');
+if (!apiUrl || !clientId || !clientSecret) {
+  console.error('ERRORE: Una variabile ambiente non è impostata');
   process.exit(1);
 }
 
-const updatedContent = envContent.replace('{API_URL}', apiUrl);
+const updatedContent = envContent.replace('{API_URL}', apiUrl)
+  .replace('{CLIENT_ID}',clientId)
+  .replace('{CLIENT_SECRET}', clientSecret);
+
+
 
 fs.writeFileSync(targetPath, updatedContent);
 
